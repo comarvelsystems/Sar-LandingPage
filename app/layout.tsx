6ee193cc731f02029c01632f6a02c07d4051296e
comarvelsystems <tkahmedkamal@marvel.com.sa>
2024-12-17 15:02:34 +0200

    Add footer branding section with logo and link to Marvel Systems

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Cairo } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import './globals.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const cairo = Cairo({
   subsets: ['arabic', 'latin'],
@@ -27,6 +29,18 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <body className={cairo.className}>
+        <div className="flex items-center justify-center gap-4 bg-black p-2 text-center text-xs font-bold text-white">
+          <Image
+            src="/marvel.png"
+            width={80}
+            height={80}
+            alt="Marvel Systems"
+          />
+          <div>
+            التطوير بواسطة
+            <Link href="https://www.marvel.com.sa"> Marvel Systems</Link>
+          </div>
+        </div>
         <Navbar />
         {children}
         <Footer />
